4a179bc268be195b45165bc888b1002831a62d75
Guilherme <Guilhermelisboa98@outlook.com>
2022-04-02 00:53:49 -0300

    Preparando para Deploy

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 
+const port = process.env.PORT || 3001;
+
 const app = express();
 app.use((req, res, nexts) => {
+  res.header('X-Powered-By', 'Express');
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST, DELETE');
+
   app.use(cors());
   nexts();
 });
 app.use(express.json());
 app.use(routes);
 
-app.listen(3001, () => console.log('server started at http://localhost:3001'));
+app.listen(port, () => console.log('server started at http://localhost:3001'));
